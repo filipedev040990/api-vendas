54c9fe86abf69137b05d2c5c939b89c62683e39b
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-14 19:27:48 -0300

    chore: Configuração do servidor e rota inicial

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,17 @@
-console.log('Iniciando aplicação')
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import routes from './routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+const SERVER_PORT = process.env.SERVER_PORT;
+
+app.listen(SERVER_PORT, console.log(`Server started on port ${SERVER_PORT}`));
