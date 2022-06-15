879f2adbeca012380b2d2cba4f7707481940b132
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-14 23:13:01 -0300

    chore: instalação do typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 dotenv.config();
 
@@ -11,6 +13,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, resquest: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 const SERVER_PORT = process.env.SERVER_PORT;
 
