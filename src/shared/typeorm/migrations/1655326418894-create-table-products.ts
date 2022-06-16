df0fb0e3ee2810ae2c85dd0b31d2ba11eaf48b41
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-15 22:32:01 -0300

    chore: cruação da entidade Products

--- src/shared/typeorm/migrations/1655326418894-create-table-products.ts
+++ src/shared/typeorm/migrations/1655326418894-create-table-products.ts
@@ -1,7 +1,8 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class createTableProducts1655325429613 implements MigrationInterface {
+export class createTableProducts1655326418894 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'products',
