24b03943a9342657be0be4fd710d38ad08e77caf
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-15 23:46:26 -0300

    feat: criando serviço ListProductService

---
+++ src/modules/products/services/ListProductsService.ts
@@ -0,0 +1,10 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+import Product from '../typeorm/entities/Products';
+
+export class ListProductsService {
+  public static async execute(): Promise<Product[]> {
+    const productRepository = getCustomRepository(ProductRepository);
+    return await productRepository.find();
+  }
+}
