1eb99ec1b29f7f40c15fb8caa95db95144e63b2b
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-17 18:21:45 -0300

    chore: Ajustando UserRepository

--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -6,4 +6,12 @@ export class UserRepository extends Repository<Users> {
   public async findByEmail(email: string): Promise<Users | undefined> {
     return this.findOne({ where: { email } });
   }
+
+  public async findByName(name: string): Promise<Users[] | undefined> {
+    return this.find({ where: { name } });
+  }
+
+  public async findById(id: string): Promise<Users | undefined> {
+    return this.findOne({ where: { id } });
+  }
 }
