3b611b2b496ecc2d7e7310c756e03ba8541c7008
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-17 18:21:05 -0300

    feat: Criando serviço de criação de users

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import { UserRepository } from '../typeorm/repositories/UsersRepository';
+import Users from '../typeorm/entities/Users';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  public static async execute({
+    name,
+    email,
+    password,
+  }: IRequest): Promise<Users> {
+    const userRepository = getCustomRepository(UserRepository);
+    const userExists = await userRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('Email adress already used');
+    }
+
+    const password_hashed = await hash(password, 10);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: password_hashed,
+    });
+    return await userRepository.save(user);
+  }
+}
