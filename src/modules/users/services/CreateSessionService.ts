e10a9a9aadf14c16717cb117931360b775f12e30
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-19 15:38:17 -0300

    feat: Criando service de Sessoes de usuario

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import Users from '../typeorm/entities/Users';
+import { UserRepository } from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+// interface IResponse {
+//   user: Users;
+//   token: '123456798';
+// }
+
+export default class CreateSessionService {
+  public static async execute({ email, password }: IRequest): Promise<Users> {
+    const userRepository = getCustomRepository(UserRepository);
+
+    const user = await userRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Invalid user/password');
+    }
+
+    const passwordIsValid = await compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new AppError('Invalid user/password');
+    }
+    return user;
+  }
+}
