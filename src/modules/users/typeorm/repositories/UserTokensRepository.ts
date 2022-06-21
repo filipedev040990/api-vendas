44dd783ee85af2f5c32de3bb05664ea12ef8c697
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-21 18:06:52 -0300

    chore: Criando repositorio UserTokens

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserTokens from '../entities/UserTokens';
+
+@EntityRepository(UserTokensRepository)
+export default class UserTokensRepository extends Repository<UserTokens> {
+  public async findByToken(token: string): Promise<UserTokens | undefined> {
+    return this.findOne({ where: { token } });
+  }
+
+  public async generate(user_id: string): Promise<UserTokens> {
+    const userToken = await this.create({ user_id });
+    return await this.save(userToken);
+  }
+}
