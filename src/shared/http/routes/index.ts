0e65809935efe448fec9ebc4581a1e94b4b04748
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-23 17:20:53 -0300

    feat: Adicionando rota de password

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import productRouter from '@modules/products/routes/products.routes';
 import authenticationRouter from '@modules/users/routes/authentication.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 import userRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/products', productRouter);
 routes.use('/users', userRouter);
 routes.use('/authentication', authenticationRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
