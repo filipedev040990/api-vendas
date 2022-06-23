6133d92332e22e3b70d8e850beca9a93cc7e81d7
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-23 17:03:02 -0300

    feat: Criando controller de ForgotPassword

---
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  public static async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body;
+    await SendForgotPasswordEmailService.execute(email);
+
+    return res.status(204).json();
+  }
+}
