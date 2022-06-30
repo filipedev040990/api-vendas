53a232687fc0a8f6b4bebe442c1aa56206c90011
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-30 17:34:40 -0300

    feat: Criando serviço de atualização de customer

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customers from '../typeorm/entities/Customers';
+import { CustomerRepository } from '../typeorm/repositories/CustomersRepositoy';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export default class UpdateCustomerService {
+  public static async execute({
+    id,
+    name,
+    email,
+  }: IRequest): Promise<Customers> {
+    const customerRepository = getCustomRepository(CustomerRepository);
+
+    const customer = await customerRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('User not found');
+    }
+
+    const emailExists = await customerRepository.findByEmail(email);
+
+    if (emailExists && emailExists.id !== id) {
+      throw new AppError('Email address already used');
+    }
+
+    customer.name = name;
+    customer.email = email;
+
+    return await customerRepository.save(customer);
+  }
+}
