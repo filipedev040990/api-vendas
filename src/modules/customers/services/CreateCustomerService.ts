148b852d6f4d9ab09d3307602200f3c17bacfb16
Filipe Siqueira <filipedev040990@gmail.com>
2022-06-30 17:25:29 -0300

    feat: Criando serviço de criação de customer

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customers from '../typeorm/entities/Customers';
+import { CustomerRepository } from '../typeorm/repositories/CustomersRepositoy';
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+export default class CreateCustomerService {
+  public static async execute({ name, email }: IRequest): Promise<Customers> {
+    const customerRepository = getCustomRepository(CustomerRepository);
+    const emailExists = await customerRepository.findByEmail(email);
+
+    if (emailExists){
+      throw new AppError('Email address already used');
+    }
+
+    const user = customerRepository.create({ name, email });
+    return await customerRepository.save(user);
+  }
+}
