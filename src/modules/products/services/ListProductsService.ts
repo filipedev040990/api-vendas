e09b1de4a9898b4e49eb94104cdc9f3bafd63ef4
Filipe Siqueira <filipedev040990@gmail.com>
2022-07-01 13:38:55 -0300

    feat: Configurando paginação na listagem de products

--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -2,9 +2,18 @@ import { getCustomRepository } from 'typeorm';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 import Product from '../typeorm/entities/Products';
 
+interface IPaginateProduct {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  data: Product[];
+}
+
 export default class ListProductsService {
-  public static async execute(): Promise<Product[]> {
+  public static async execute(): Promise<IPaginateProduct> {
     const productRepository = getCustomRepository(ProductRepository);
-    return await productRepository.find();
+    return await productRepository.createQueryBuilder().paginate();
   }
 }
