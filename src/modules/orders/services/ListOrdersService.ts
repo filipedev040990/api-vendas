00e41b0328a328f933ebb4b7b8bb64eccb4350bb
Filipe Siqueira <filipedev040990@gmail.com>
2022-07-01 13:36:22 -0300

    feat: Configurando paginação na listagem de orders

--- src/modules/orders/services/ListOrdersService.ts
+++ src/modules/orders/services/ListOrdersService.ts
@@ -2,9 +2,18 @@ import { getCustomRepository } from 'typeorm';
 import Orders from '../typeorm/entities/Orders';
 import OrdersRepository from '../typeorm/repositories/OrdersRepository';
 
+interface IPaginateOrders {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  data: Orders[];
+}
+
 export default class ListOrderService {
-  public static async execute(): Promise<Orders[]> {
+  public static async execute(): Promise<IPaginateOrders> {
     const orderRepository = getCustomRepository(OrdersRepository);
-    return await orderRepository.find();
+    return await orderRepository.createQueryBuilder().paginate();
   }
 }
