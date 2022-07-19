77ebb4964218cdc1b2d19a23e7cf2284eec5a877
Filipe Siqueira <filipedev040990@gmail.com>
2022-07-19 17:39:08 -0300

    Criando camada de infra de Orders

---
+++ src/modules/orders/infra/http/controllers/OrdersController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import CreateOrderService from '../../../services/CreateOrderService';
+import ListOrderByIdService from '../../../services/ListOrderByIdService';
+import ListOrderService from '../../../services/ListOrdersService';
+
+export default class OrdersController {
+  public static async index(req: Request, res: Response): Promise<Response> {
+    const orders = await ListOrderService.execute();
+    return res.status(200).json(orders);
+  }
+
+  public static async listById(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const order = await ListOrderByIdService.execute(id);
+    return res.status(200).json(order);
+  }
+
+  public static async create(req: Request, res: Response): Promise<Response> {
+    const { customer_id, products } = req.body;
+    const order = await CreateOrderService.execute({ customer_id, products });
+    return res.status(200).json(order);
+  }
+}
---
+++ src/modules/orders/infra/http/routes/orders.routes.ts
@@ -0,0 +1,33 @@
+import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import OrdersController from '../controllers/OrdersController';
+
+const orderRouter = Router();
+
+orderRouter.get('/', isAuthenticated, OrdersController.index);
+
+orderRouter.get(
+  '/:id',
+  isAuthenticated,
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  OrdersController.listById,
+);
+
+orderRouter.post(
+  '/',
+  isAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      customer_id: Joi.string().uuid().required(),
+      products: Joi.required(),
+    },
+  }),
+  OrdersController.create,
+);
+
+export default orderRouter;
