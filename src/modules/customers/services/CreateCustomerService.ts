cf3ea8c05eb9ed7d45cf50f38e16ec86ee093226
Filipe Siqueira <filipedev040990@gmail.com>
2022-07-19 18:00:42 -0300

    refact: Criando camada de dominio e interfacers de Customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,15 +1,14 @@
+import { ICreateCustomer } from './../domain/models/ICreateCustomer';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Customers from '../infra/typeorm/entities/Customers';
 import { CustomerRepository } from '../infra/typeorm/repositories/CustomersRepositoy';
 
-interface IRequest {
-  name: string;
-  email: string;
-}
-
 export default class CreateCustomerService {
-  public static async execute({ name, email }: IRequest): Promise<Customers> {
+  public static async execute({
+    name,
+    email,
+  }: ICreateCustomer): Promise<Customers> {
     const customerRepository = getCustomRepository(CustomerRepository);
     const emailExists = await customerRepository.findByEmail(email);
 
