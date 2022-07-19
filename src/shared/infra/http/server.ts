b4791e7f1e5db3561102c408a22ea6cd055ee7c5
Filipe Siqueira <filipedev040990@gmail.com>
2022-07-19 17:29:46 -0300

    refact: Criando camada de infra em shared

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,46 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import { errors } from 'celebrate';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+import uploadConfig from '@config/upload';
+import { pagination } from 'typeorm-pagination';
+import ratelimiter from '@shared/infra/http/middlewares/rateLimiter';
+
+dotenv.config();
+
+const app = express();
+
+app.use(ratelimiter);
+
+app.use(cors());
+app.use(express.json());
+app.use(pagination);
+app.use('/files', express.static(uploadConfig.directory));
+
+app.use(routes);
+app.use(errors());
+
+app.use(
+  (error: Error, resquest: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.log(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+const SERVER_PORT = process.env.SERVER_PORT;
+
+app.listen(SERVER_PORT, console.log(`Server started on port ${SERVER_PORT}`));
