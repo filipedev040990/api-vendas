df11cc865c69f77fda52bac181779dbb81054aad
Filipe Siqueira <filipe@desktop-filipe.localdomain>
2022-08-02 18:39:13 -0300

    feat: Implementando validação de quantidade e preço no serviço de criação de produto

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -15,7 +15,7 @@ export default class CreateProductService {
   constructor(
     @inject('ProductRepository')
     private productRepository: IProductRepository,
-  ) {}
+  ) { }
   public async execute({
     name,
     price,
@@ -25,6 +25,14 @@ export default class CreateProductService {
 
     if (productExists) {
       throw new AppError('There is already product with this name');
+    }
+
+    if (price < 0) {
+      throw new AppError('Price is invalid');
+    }
+
+    if (quantity < 0) {
+      throw new AppError('Quantity is invalid');
     }
 
     const redisCache = new RedisCache();
